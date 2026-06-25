8b5c2af00e3c537596e42c1f16f98aa5f8529f46
behnam0032 <behnam0032@gmail.com>
2026-06-25 12:19:13 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,113 +1,91 @@
 const API_URL =
 "https://Api.BrsApi.ir/Market/Gold_Currency.php?key=BSzSYFaRH8NcdMi7YpLdmmHYbmRjdlx7";
 
-function numberFormat(num){
 
-return Number(num).toLocaleString('fa-IR');
 
+function format(num){
+    return Number(num).toLocaleString('fa-IR');
 }
 
-function setStatus(id, open){
+function setStatus(id,isOpen){
 
-const el = document.getElementById(id);
+    const el=document.getElementById(id);
 
-if(open){
-
-el.className="status open";
-el.innerText="باز";
-
-}else{
-
-el.className="status closed";
-el.innerText="بسته";
-
+    if(isOpen){
+        el.className="status open";
+        el.innerText="باز";
+    }else{
+        el.className="status closed";
+        el.innerText="بسته";
+    }
 }
 
-}
+function checkOpen(start,end,h){
 
-function isOpen(start,end,hour){
+    if(start<end){
+        return h>=start && h<end;
+    }
 
-if(start < end){
-
-return hour >= start && hour < end;
-
-}
-
-return hour >= start || hour < end;
-
+    return h>=start || h<end;
 }
 
 function updateSessions(){
 
-const now = new Date();
+    const now=new Date();
 
-const h = now.getUTCHours();
-const m = now.getUTCMinutes();
-const s = now.getUTCSeconds();
+    const h=now.getUTCHours();
+    const m=now.getUTCMinutes();
+    const s=now.getUTCSeconds();
 
-document.getElementById("utc-clock").innerText =
-`UTC : ${h}:${m}:${s}`;
+    document.getElementById("utc-clock").innerText=
+    `UTC ${String(h).padStart(2,'0')}:${String(m).padStart(2,'0')}:${String(s).padStart(2,'0')}`;
 
-setStatus("london",isOpen(8,17,h));
-setStatus("newyork",isOpen(13,22,h));
-setStatus("tokyo",isOpen(0,9,h));
-setStatus("sydney",isOpen(22,7,h));
-
+    setStatus("london",checkOpen(8,17,h));
+    setStatus("newyork",checkOpen(13,22,h));
+    setStatus("tokyo",checkOpen(0,9,h));
+    setStatus("sydney",checkOpen(22,7,h));
 }
 
-async function getMarketData(){
+async function loadMarket(){
 
-try{
+    try{
 
-const response = await fetch(API_URL);
+        const response=await fetch(API_URL);
 
-const data = await response.json();
+        const data=await response.json();
 
-const xau =
-data.gold.find(x=>x.symbol==="XAUUSD");
+        const xau=data.gold.find(i=>i.symbol==="XAUUSD");
+        const gold18=data.gold.find(i=>i.symbol==="IR_GOLD_18K");
+        const melted=data.gold.find(i=>i.symbol==="IR_GOLD_MELTED");
 
-const gold18 =
-data.gold.find(x=>x.symbol==="IR_GOLD_18K");
+        const usd=data.currency.find(i=>i.symbol==="USD");
+        const usdt=data.currency.find(i=>i.symbol==="USDT_IRT");
 
-const melted =
-data.gold.find(x=>x.symbol==="IR_GOLD_MELTED");
+        const emami=data.coin.find(i=>i.symbol==="EMAMI");
+        const nim=data.coin.find(i=>i.symbol==="NIM");
 
-const usd =
-data.currency.find(x=>x.symbol==="USD");
+        if(xau) document.getElementById("xau").innerText=format(xau.price);
+        if(usd) document.getElementById("usd").innerText=format(usd.price);
+        if(usdt) document.getElementById("usdt").innerText=format(usdt.price);
 
-const usdt =
-data.currency.find(x=>x.symbol==="USDT_IRT");
+        if(gold18) document.getElementById("gold18").innerText=format(gold18.price);
+        if(melted) document.getElementById("melted").innerText=format(melted.price);
 
-document.getElementById("xau").innerText =
-numberFormat(xau.price);
+        if(emami) document.getElementById("emami").innerText=format(emami.price);
+        if(nim) document.getElementById("nim").innerText=format(nim.price);
 
-document.getElementById("gold18").innerText =
-numberFormat(gold18.price);
+        document.getElementById("last-update").innerText =
+        new Date().toLocaleTimeString('fa-IR');
 
-document.getElementById("melted").innerText =
-numberFormat(melted.price);
+    }catch(error){
 
-document.getElementById("usd").innerText =
-numberFormat(usd.price);
+        console.log(error);
 
-document.getElementById("usdt").innerText =
-numberFormat(usdt.price);
-
-document.getElementById("last-update").innerText =
-new Date().toLocaleTimeString('fa-IR');
-
-}
-catch(error){
-
-console.log(error);
-
-}
-
+    }
 }
 
 updateSessions();
-getMarketData();
+loadMarket();
 
 setInterval(updateSessions,1000);
-
-setInterval(getMarketData,60000);
+setInterval(loadMarket,60000);
