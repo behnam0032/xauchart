8ef376a41b7fd0373267886bd34c6637ac5774a7
behnam <Behnam0032@gmail.com>
2026-08-09 22:22:08 +0330

    Updates

---
+++ assets/calc-D5fwvTDK.js
@@ -0,0 +1,9 @@
+import{r as a,i,a as e}from"./clock-DyX6SMzh.js";/* empty css                */import{g as r,i as t}from"./calculator-BNxaeBxc.js";document.getElementById("app").innerHTML=a("calc")+`
+<main class="main">
+  <div class="page-header"><h1>🧮 ماشین حساب فارکس</h1><p>
+    با ماشین حساب‌های معاملاتی XAUChart می‌توانید
+    لات، مارجین، سود و زیان، میانگین ورود، سود مرکب
+    و سایر محاسبات موردنیاز معاملات فارکس و طلا را محاسبه کنید.
+</p></div>
+  ${r()}
+</main>`;i();e();t();
--- assets/calc-nu0MTspV.js
+++
@@ -1,5 +0,0 @@
-import{r as a,i,a as e}from"./clock-DyX6SMzh.js";/* empty css                */import{g as r,i as n}from"./calculator-BNxaeBxc.js";document.getElementById("app").innerHTML=a("calc")+`
-<main class="main">
-  <div class="page-header"><h1>🧮 ماشین حساب فارکس</h1><p>ابزارهای حرفه‌ای محاسبه حجم، مارجین، سود/زیان و مدیریت ریسک</p></div>
-  ${r()}
-</main>`;i();e();n();
